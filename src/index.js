d1592f75c7c395828fd56a00757359d113ddd05d
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-08-03 20:02:36 +0300

    feat: add solve task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const regExMask = /.{10}/g;
+
+    let result = expr.match(regExMask);
+
+    result = result.map((word) => {
+         return decodeSymbol(word);
+    });
+
+    function decodeSymbol(symbol) {
+        const codeWhiteSpace = '**********';
+        const whitespace = ' ';
+
+        if (symbol === codeWhiteSpace) {
+            return whitespace;
+        }
+
+        const regExEmpty = /00/g;        
+        const regExDot = /10/g;
+        const regExDash = /11/g;
+        const EMPTY = '';
+        const DOT = '.';
+        const DASH = '-';
+
+        let result = symbol.replace(regExEmpty, EMPTY);
+        result = result.replace(regExDot, DOT);
+        result = result.replace(regExDash, DASH);
+        
+        return MORSE_TABLE[result];
+    }
+
+    return  result.join('');
 }
 
 module.exports = {
     decode
 }
+
